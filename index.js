9c53d7c0380fc39a25b871279a54222e34a7fc93
luigithiago <luigithiago13@hotmail.com>
2025-02-16 22:54:30 -0400

    commit da versão final do projeto final

--- index.js
+++ index.js
@@ -1,3 +1,34 @@
-let first = "Hello World"
+class Hero{
+    constructor(name, age, role){
+        this.name = name
+        this.age = age
+        this.role = role
+    }
 
-console.log(first)
+    attack(){
+        //Objeto para mapear roles e ataques
+        const attackMap = {
+            "Mage": "Spell",
+            "Warrior": "Sword",
+            "Monk": "Ninjutsu",
+            "Ninja": "Stealth Attacks"
+        }
+
+        //Ataque mapeado na saída ou default case
+        const attack = attackMap[this.role] || "Unknown attack"
+
+        console.log(`O ${this.role} atacou usando ${attack}!\n`)
+    
+    }
+
+}
+
+const hero1 = new Hero("Gandalf" , 82, "Mage")
+const hero2 = new Hero("Arthas" , 189, "Warrior")
+const hero3 = new Hero("Aang", 12, "Monk")
+const hero4 = new Hero("Ryu Hayabusa" , 37, "Ninja")
+
+hero1.attack()
+hero2.attack()
+hero3.attack()
+hero4.attack()
